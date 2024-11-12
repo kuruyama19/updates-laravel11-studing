7eed3e5d51933a3ce7dc60ae0d6d916f83f4bbf1
Vinícius Vieira <vinicius_cs16@hotmail.com>
2024-11-11 22:11:08 -0300

    Att: Snippets do blade

--- 001_notes/routes/web.php
+++ 001_notes/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 Route::get('/about', function () {
     echo 'About Us!';
 });
+Route::get('/main/{value}',[MainController::class, 'index']);
